5af4052a72683bb7c5ba42e820f88aab34e00c19
Glitch (roomy-boggy-silverfish) <none>
2020-12-02 23:57:19 +0000

    Import of alvinashiatey/aaa

---
+++ src/tema/js/tema.js
@@ -0,0 +1,59 @@
+import "../sass/tema.scss";
+
+function headingLoc() {
+  const image = document.getElementById("image");
+  const tema = {
+    lat: 5.6249375,
+    lon: 0.0000625,
+  };
+
+  const range = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
+  function angleFromCoordinate(lat1, lon1, lat2, lon2) {
+    var p1 = {
+      x: lat1,
+      y: lon1,
+    };
+
+    var p2 = {
+      x: lat2,
+      y: lon2,
+    };
+
+    var angleDeg = (Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180) / Math.PI;
+    return angleDeg;
+  }
+
+  navigator.geolocation.watchPosition((data) => {
+    console.log(data.coords);
+    let opacityRng = range(data.coords.heading, 0, 360, 0, 100);
+    image.style.opacity = `${opacityRng}%`;
+  });
+}
+
+function imgSlider() {
+  let counter = 1;
+  function move() {
+    const container = document.getElementById("image");
+    const img = document.createElement("img");
+    if (counter <= 100) {
+      container.innerHTML = "";
+      img.setAttribute("src", `img/t/img_${counter}.png`);
+      container.appendChild(img);
+      counter++;
+    } else {
+      counter = 1;
+    }
+    setTimeout(move, 10000);
+  }
+  move();
+}
+
+function init() {
+  imgSlider();
+  headingLoc();
+}
+
+init();
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/js/index.js",
+    tema: "./src/tema/js/tema.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: `js/[name].bundle.js`,
+  },
+  devServer: {
+    contentBase: "./public",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "index.html",
+      template: "./src/index.html",
+      chunks: ["main"],
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "tema.html",
+      template: "./src/tema.html",
+      chunks: ["tema"],
+    }),
+    new CopyPlugin([{ from: "./src/tema/img", to: "img/t/" }]),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.s?[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "cheap-module-source-map",
+};
